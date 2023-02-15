9929fd94882243dafc806998f0464bcfc2771e45
hamza19mughal99 <hamza19mughal99@gmail.com>
2023-02-16 00:17:25 +0500

    origin add

--- app.js
+++ app.js
@@ -8,8 +8,13 @@ import handleError from './middlewares/handleError.js';
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
 app.use(bodyParser.json());
-app.use(cors()); 
+app.use(cors());
 
 app.use("/iframe", candidateRoute);
 app.use(handleError)
